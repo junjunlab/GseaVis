133ef624c9fe71c2851c42a37c19379a4da693f4
JunJun <3219030654@stu.cpu.edu.cn>
2024-11-07 14:02:25 +0800

    update

--- R/sankeyGoPlot.R
+++ R/sankeyGoPlot.R
@@ -6,12 +6,15 @@ globalVariables(c("gene_ratio", 'next_node', "next_x", "node", "term_ratio"))
 #' @param topGenes the top genes to be shown for each term, default 5.
 #' @param keep_all_gene Whether keep all genes, default "FALSE", `topGenes` will be useless if TRUE.
 #' @param sankeyExpand the sankey plot expand for left and side, default c(0.5,1).
+#' @param flow_fill The flow fill color, default `grey`.
 #' @param nodeSize node text size for sankey plot, default 2.5.
 #' @param nodeColor node fill color for sankey plot, default NULL.
 #' @param goCol go color for go plot, default NULL.
 #' @param xShift the shift on horizontal for go plot, default 0.05.
 #' @param downShift the shift on vertical for go plot, default 4.5.
 #' @param upShift the shift on vertical for go plot, default 0.25.
+#' @param geom_layer Custom geom_layers, default NULL.
+#' @param enrich_type The enrichment type, "go" or "gsea".
 #'
 #' @return ggplot obeject
 #'
@@ -24,27 +27,40 @@ sankeyGoPlot <- function(goData = NULL,
                          topGenes = 5,
                          keep_all_gene = FALSE,
                          sankeyExpand = c(0.5,1),
+                         flow_fill = "grey",
                          nodeSize = 2.5,
                          nodeColor = NULL,
                          goCol = NULL,
                          xShift = 0.05,
                          downShift = 4.5,
-                         upShift = 0.25){
+                         upShift = 0.25,
+                         geom_layer = NULL,
+                         enrich_type = c("go","gsea")){
+  enrich_type <- match.arg(enrich_type,c("go","gsea"))
   # ============================================================================
   # 1.prepare data
   # ============================================================================
-  ego_df <- goData %>%
-    dplyr::group_by(Description) %>%
-    dplyr::mutate(gene_ratio = eval(parse(text = GeneRatio))) %>%
-    dplyr::arrange(pvalue)
+  if(enrich_type == "gsea"){
+    ego_df <- goData %>%
+      dplyr::rename(geneID = core_enrichment) %>%
+      dplyr::group_by(Description) %>%
+      dplyr::arrange(pvalue)
+  }else{
+    ego_df <- goData %>%
+      dplyr::group_by(Description) %>%
+      dplyr::mutate(gene_ratio = eval(parse(text = GeneRatio))) %>%
+      dplyr::arrange(pvalue)
+
+    # calculate term_ratio(diffGenes / termGenes in pathway)
+    ego_df$term_gene <- sapply(strsplit(ego_df$BgRatio,split = "\\/"),"[",1) %>%
+      as.numeric()
+    ego_df$term_ratio <- ego_df$Count/ego_df$term_gene
+  }
+
 
   # term order
   ego_df$Description <- factor(ego_df$Description,levels = rev(ego_df$Description))
 
-  # calculate term_ratio(diffGenes / termGenes in pathway)
-  ego_df$term_gene <- sapply(strsplit(ego_df$BgRatio,split = "\\/"),"[",1) %>%
-    as.numeric()
-  ego_df$term_ratio <- ego_df$Count/ego_df$term_gene
 
   # select genes from every term
   sankey_df <- ego_df %>%
@@ -86,9 +102,9 @@ sankeyGoPlot <- function(goData = NULL,
                          fill = factor(node),
                          label = node)) +
     geom_sankey(flow.alpha = 0.5,
-                flow.fill = 'grey',
-                flow.color = 'grey',
-                width = 0.1,
+                flow.fill = flow_fill,
+                # flow.color = 'grey',
+                # width = 0.1,
                 node.fill = mycol) +
     theme_void() +
     scale_x_discrete(expand = expansion(mult = sankeyExpand)) +
@@ -114,11 +130,23 @@ sankeyGoPlot <- function(goData = NULL,
     pcol <- scale_fill_gradient(low = goCol[1],high = goCol[2])
   }
 
+  if(!is.null(geom_layer)){
+    plyer <- geom_layer
+  }else{
+    if(enrich_type == "gsea"){
+      plyer <- geom_point(aes(x = -log10(pvalue),y = Description,
+                              size = -log10(p.adjust),fill = -log10(p.adjust)),
+                          color = "black",shape = 21)
+    }else{
+      plyer <- geom_point(aes(x = -log10(pvalue),y = Description,
+                              size = term_ratio,fill = gene_ratio),
+                          color = "black",shape = 21)
+    }
+  }
+
   pp <-
     ggplot(ego_df) +
-    geom_point(aes(x = -log10(pvalue),y = Description,
-                   size = term_ratio,fill = gene_ratio),
-               color = "black",shape = 21) +
+    plyer +
     theme_bw() +
     theme(axis.text.y = element_blank(),
           axis.ticks.y = element_blank(),
